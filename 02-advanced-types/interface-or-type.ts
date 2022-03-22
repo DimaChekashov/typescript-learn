d108bb99a2262187ba505f341c6dc4a8ea6b1fd1
DimaChekashov <foxdima99@gmail.com>
2022-03-22 15:58:34 +0300

    add interface-or-type

---
+++ 02-advanced-types/interface-or-type.ts
@@ -0,0 +1,18 @@
+interface UserFour {
+    name: string
+}
+
+interface UserFour {
+    age: number;
+}
+
+const user_4: UserFour = {
+    name: 'Dunkan',
+    age: 22
+}
+
+type ID = string | number;
+
+interface IDI {
+    ID: string | number
+}
