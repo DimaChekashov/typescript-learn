6af6aef93a25b585b194a9f57f7b9ef4d63310e0
DimaChekashov <foxdima99@gmail.com>
2022-03-22 16:30:36 +0300

    complete optionals

---
+++ 02-advanced-types/optional.ts
@@ -0,0 +1,32 @@
+interface Block {
+    login: string;
+    password?: string;
+}
+
+const block: Block = {
+    login: 'a@a.ru',
+    password: '1'
+}
+
+function multiply(first: number, second?: number): number {
+    if(!second) {
+        return first * first;
+    }
+    
+    return first * second;
+}
+
+interface BlockPro {
+    login: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    }
+}
+
+function testPass(block: BlockPro) {
+    const t = block.password?.type;
+}
+
+function test(param?: string) {
+    const t = param ?? multiply(5);
+}
