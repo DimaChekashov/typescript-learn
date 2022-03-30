13ffa4cf2e5b203eba9cea19ec38c7d426f39175
DimaChekashov <foxdima99@gmail.com>
2022-03-30 19:05:45 +0300

    completed test 6

---
+++ 03-classes/test6/test6.js
@@ -0,0 +1,45 @@
+"use strict";
+// Необходимо реализовать абстрактный класс Logger с 2-мя методами
+// абстрактным - log(message): void
+// printDate - выводящий в log дату
+// К нему необходимо сделать реальный класс, который бы имел метод: logWithDate,
+// выводящий сначала дату, а потом заданное сообщение
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Logger = /** @class */ (function () {
+    function Logger() {
+    }
+    Logger.prototype.printDate = function (date) {
+        this.log(date.toString());
+    };
+    return Logger;
+}());
+var Weather = /** @class */ (function (_super) {
+    __extends(Weather, _super);
+    function Weather() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Weather.prototype.log = function (message) {
+        console.log(message);
+    };
+    Weather.prototype.logWithDate = function (message) {
+        this.printDate(new Date());
+        this.log(message);
+    };
+    return Weather;
+}(Logger));
+var weather = new Weather();
+weather.logWithDate("Небольшой дождь!");
