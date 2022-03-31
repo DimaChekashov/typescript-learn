c5da13b7d7a2dc8b5236110874265fd90aeeba5b
DimaChekashov <foxdima99@gmail.com>
2022-03-31 16:02:41 +0300

    completed test 7

---
+++ 05-generics/test7/test7.ts
@@ -0,0 +1,29 @@
+function testToString<T>(data: T): string | undefined {
+    if(Array.isArray(data)) {
+        return data.toString();
+    }
+
+    switch(typeof data) {
+        case "string":
+            return data;
+        case "number":
+        case "symbol":
+        case "bigint":
+        case "boolean":
+        case "function":
+            return data.toString();
+        case "object":
+            return JSON.stringify(data);
+        default:
+            return undefined;
+    }
+}
+
+console.log(testToString([1, 2, 3]));
+console.log(testToString(true));
+console.log(testToString(null));
+console.log(testToString(12312312));
+console.log(testToString({
+    id: 1,
+    name: 'ban'
+}));
