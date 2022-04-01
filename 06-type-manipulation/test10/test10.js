176134f1b2c93b2ff0085d1269cbe9cf1dc2502b
DimaChekashov <foxdima99@gmail.com>
2022-04-01 16:50:30 +0300

    completed test 10

---
+++ 06-type-manipulation/test10/test10.js
@@ -0,0 +1,33 @@
+"use strict";
+// Необходимо написать функцию группировки, которая принимает массив объектов
+// и его ключ, производит группировку по указанному ключу и возращает
+// сгруппированный объект.
+// Пример:
+// ``` js
+// [
+// 	{ group: 1, name: 'a' },
+// 	{ group: 1, name: 'b' },
+// 	{ group: 2, name: 'c' },
+// ];
+// ```
+var tdata = [
+    { group: 1, name: 'a' },
+    { group: 1, name: 'b' },
+    { group: 2, name: 'c' },
+];
+function group(array, key) {
+    return array.reduce(function (map, item) {
+        var itemKey = item[key];
+        var curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        }
+        else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+var ttres = group(tdata, 'group');
+console.log(ttres);
---
+++ 06-type-manipulation/test10/test10.ts
@@ -0,0 +1,54 @@
+// Необходимо написать функцию группировки, которая принимает массив объектов
+// и его ключ, производит группировку по указанному ключу и возращает
+// сгруппированный объект.
+// Пример:
+// ``` js
+// [
+// 	{ group: 1, name: 'a' },
+// 	{ group: 1, name: 'b' },
+// 	{ group: 2, name: 'c' },
+// ];
+// ```
+
+// При группироке по 'group' ---->
+
+// ``` js
+// {
+// 	'1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
+// 	'2': [ { group: 2, name: 'c' } ]
+// }
+// ```
+
+interface TData {
+    group: number;
+    name: string;
+}
+
+const tdata: TData[] = [
+	{ group: 1, name: 'a' },
+	{ group: 1, name: 'b' },
+	{ group: 2, name: 'c' },
+];
+
+interface IGroup<T> {
+    [key: string]: T[];
+}
+
+type key = string | number | symbol;
+
+function group<T extends Record<string, any>>(array: T[], key: keyof T): IGroup<T> {
+    return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+        const itemKey = item[key];
+        let curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        } else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+
+const ttres = group<TData>(tdata, 'group');
+console.log(ttres);
