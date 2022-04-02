78be26de16f6f8416ae04000462539859f49d477
DimaChekashov <foxdima99@gmail.com>
2022-04-02 15:21:22 +0300

    completed typeof

---
+++ 06-type-manipulation/typeof.js
@@ -0,0 +1,23 @@
+"use strict";
+var strOrNum;
+if (Math.random() > 0.5) {
+    strOrNum = 5;
+}
+else {
+    strOrNum = "str";
+}
+if (typeof strOrNum === 'string') {
+    console.log(strOrNum);
+}
+else {
+    console.log(strOrNum);
+}
+var str2OrNum;
+var tyuser = {
+    name: "Vasya"
+};
+var TDirection;
+(function (TDirection) {
+    TDirection[TDirection["Up"] = 0] = "Up";
+    TDirection[TDirection["Down"] = 1] = "Down";
+})(TDirection || (TDirection = {}));
